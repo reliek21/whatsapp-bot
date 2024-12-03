b903d7478d39d702b81d01081467f502f488f045
Keiler Guardo <keilerguardo@gmail.com>
2024-12-02 23:40:16 -0500

    feat: adding eslint

--- src/app.ts
+++ src/app.ts
@@ -61,53 +61,14 @@ const fullSamplesFlow = addKeyword<Provider, Database>(['samples', utils.setEven
 const main = async () => {
   const adapterFlow = createFlow([welcomeFlow, registerFlow, fullSamplesFlow])
 
-  const adapterProvider = createProvider(Provider)
+  // const adapterProvider = createProvider(Provider)
   const adapterDB = new Database()
 
-  const { handleCtx, httpServer } = await createBot({
+  const { httpServer } = await createBot({
     flow: adapterFlow,
-    provider: adapterProvider,
+    provider: createProvider(Provider),
     database: adapterDB,
-  })
-
-  adapterProvider.server.post(
-    '/v1/messages',
-    handleCtx(async (bot, req, res) => {
-      const { number, message, urlMedia } = req.body
-      await bot.sendMessage(number, message, { media: urlMedia ?? null })
-      return res.end('sended')
-    })
-  )
-
-  adapterProvider.server.post(
-    '/v1/register',
-    handleCtx(async (bot, req, res) => {
-      const { number, name } = req.body
-      await bot.dispatch('REGISTER_FLOW', { from: number, name })
-      return res.end('trigger')
-    })
-  )
-
-  adapterProvider.server.post(
-    '/v1/samples',
-    handleCtx(async (bot, req, res) => {
-      const { number, name } = req.body
-      await bot.dispatch('SAMPLES', { from: number, name })
-      return res.end('trigger')
-    })
-  )
-
-  adapterProvider.server.post(
-    '/v1/blacklist',
-    handleCtx(async (bot, req, res) => {
-      const { number, intent } = req.body
-      if (intent === 'remove') bot.blacklist.remove(number)
-      if (intent === 'add') bot.blacklist.add(number)
-
-      res.writeHead(200, { 'Content-Type': 'application/json' })
-      return res.end(JSON.stringify({ status: 'ok', number, intent }))
-    })
-  )
+  });
 
   httpServer(+PORT)
 }
