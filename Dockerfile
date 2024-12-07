759d8ad7528d278fb88e9487cba15a43d5258769
Keiler Guardo <keilerguardo@gmail.com>
2024-12-06 22:51:30 -0500

    feat: impproving messages

--- Dockerfile
+++ Dockerfile
@@ -1,39 +1,44 @@
-# Image size ~ 400MB
+# Builder Stage
 FROM node:21-alpine3.18 as builder
 
+# Set working directory
 WORKDIR /app
 
+# Enable Corepack and prepare PNPM
 RUN corepack enable && corepack prepare pnpm@latest --activate
+
+# Environment variables
 ENV PNPM_HOME=/usr/local/bin
 
-COPY . .
-
+# Copy only necessary files
 COPY package*.json *-lock.yaml ./
 
-RUN apk add --no-cache --virtual .gyp \
-        python3 \
-        make \
-        g++ \
-    && apk add --no-cache git \
-    && pnpm install && pnpm run build \
-    && apk del .gyp
+# Install dependencies and build
+RUN apk add --no-cache --virtual .build-deps \
+    python3 make g++ git \
+    && pnpm install --frozen-lockfile \
+    && pnpm run build \
+    && apk del .build-deps
 
+# Final Stage
 FROM node:21-alpine3.18 as deploy
 
+# Set working directory
 WORKDIR /app
 
-ARG PORT
-ENV PORT $PORT
-EXPOSE $PORT
-
+# Copy built files and production dependencies
 COPY --from=builder /app/dist ./dist
-COPY --from=builder /app/*.json /app/*-lock.yaml ./
+COPY --from=builder /app/package*.json /app/*-lock.yaml ./
 
-RUN corepack enable && corepack prepare pnpm@latest --activate 
-ENV PNPM_HOME=/usr/local/bin
+# Enable Corepack and prepare PNPM
+RUN corepack enable && corepack prepare pnpm@latest --activate
 
-RUN npm cache clean --force && pnpm install --production --ignore-scripts \
-    && addgroup -g 1001 -S nodejs && adduser -S -u 1001 nodejs \
-    && rm -rf $PNPM_HOME/.npm $PNPM_HOME/.node-gyp
+# Install production dependencies
+RUN pnpm install --production --ignore-scripts \
+    && addgroup -g 1001 -S nodejs \
+    && adduser -S -u 1001 -G nodejs nodejs \
+    && rm -rf /usr/local/share/.cache /root/.npm /root/.node-gyp
 
-CMD ["npm", "start"]
+# Set user and start command
+USER nodejs
+CMD ["node", "dist/app.js"]
--- src/app.ts
+++ src/app.ts
@@ -1,98 +1,142 @@
-import { MemoryDB as Database } from '@builderbot/bot';
-import FlowClass from '@builderbot/bot/dist/io/flowClass';
-import { BaileysProvider as Provider } from '@builderbot/provider-baileys';
-import { createBot, createProvider, createFlow, addKeyword } from '@builderbot/bot';
+import { MemoryDB as Database } from "@builderbot/bot";
+import FlowClass from "@builderbot/bot/dist/io/flowClass";
+import { BaileysProvider as Provider } from "@builderbot/provider-baileys";
+import {
+  createBot,
+  createProvider,
+  createFlow,
+  addKeyword,
+} from "@builderbot/bot";
 
 // standard messages
-const standardMessages: string = 'Mi nombre es Sofía y seré su asesora el día de hoy, ¿con quién tengo el gusto de hablar?';
+const standardMessages: string =
+  "Mi nombre es Sofía y seré su asesora el día de hoy, ¿con quién tengo el gusto de hablar?";
 
 // get the current time to determine the greeting flow
 const getGreetingFlow = (): string => {
   const hour = new Date().getHours();
-  if (hour >= 5 && hour < 12) return 'morningFlow';
-  if (hour >= 12 && hour < 18) return 'afternoonFlow';
-  return 'nightFlow';
+  if (hour >= 5 && hour < 12) return "morningFlow";
+  if (hour >= 12 && hour < 18) return "afternoonFlow";
+  return "nightFlow";
 };
 
 const greetingStandard: string | [string, ...string[]] = [
-  'saludo', 'saludar', 'saludos', 'hola',
-  'buenos dias', 'buenos días',
-  'buenas tardes', 'buenas noches',
-  'buenos días', 'dias', 'buenos dias', 'hola, buenos dias',
-  'buenas tardes', 'tardes', 'hola, buenas tardes',
-  'buenas noches', 'noches', 'hola, buenas noches',
-  'hola, tienes disponibilidad',
-  'disponibilidad',
-  'tienes disponibilidad',
-  '¡Hola! 😊 Tropecé con su sitio web y me picó la curiosidad',
-  'Hola, tienes',
+  "saludo",
+  "saludar",
+  "saludos",
+  "hola",
+  "buenos dias",
+  "buenos días",
+  "buenas tardes",
+  "buenas noches",
+  "buenos días",
+  "dias",
+  "buenos dias",
+  "hola, buenos dias",
+  "buenas tardes",
+  "tardes",
+  "hola, buenas tardes",
+  "buenas noches",
+  "noches",
+  "hola, buenas noches",
+  "hola, tienes disponibilidad",
+  "disponibilidad",
+  "tienes disponibilidad",
+  "¡Hola! 😊 Tropecé con su sitio web y me picó la curiosidad. 🌐 ¿Podrían compartirme más detalles sobre sus productos? 🛍️ ¡Gracias! 🌟",
+  "Hola, tienes",
 ];
 
 // dynamic greeting flow
 const dynamicGreetingFlow = addKeyword<Provider, Database>(greetingStandard)
   .addAction(async (_, { flowDynamic }) => {
     const greetingFlow = getGreetingFlow();
-    if (greetingFlow === 'morningFlow') {
-      return flowDynamic('Hola, Buenos días 🌞');
-    } else if (greetingFlow === 'afternoonFlow') {
-      return flowDynamic('Hola, Buenas tardes 🌞');
+    if (greetingFlow === "morningFlow") {
+      return flowDynamic("Hola, Buenos días 🌞");
+    } else if (greetingFlow === "afternoonFlow") {
+      return flowDynamic("Hola, Buenas tardes 🌞");
     } else {
-      return flowDynamic('Hola, Buenas noches 🌜');
+      return flowDynamic("Hola, Buenas noches 🌜");
     }
-  }).addAnswer(standardMessages, { delay: 2000, capture: true })
+  })
+  .addAnswer(standardMessages, { delay: 2000, capture: true })
   .addAction(async (ctx, { flowDynamic, state }) => {
     const name: string = ctx.body.toLowerCase();
     await state.update({ name });
 
     return await flowDynamic(
       `Hola, ${name}, Bienvenid@ a Divino Placer 😊, ¿En qué puedo ayudarte el día de hoy?`,
-      { delay: 5000 }
+      { delay: 5000 },
     );
-  }).addAnswer('¿Estas en busca de un producto en especifico?', { delay: 3000 });
+  })
+  .addAnswer("¿Estas en busca de un producto en especifico?", { delay: 3000 });
 
 const cityFlow = addKeyword<Provider, Database>([
-  'donde estan ubicados', 'dónde están ubicados',
-  'ubicacion', 'ubicación',
-  'direccion', 'dirección',
-  '¿Hacen envíos?',
-  'envios',
-  'hacen envios',
-  'envios a domicilio',
-  'ubicacion', 'ubicación',
-  'donde estan ubicados', 'dónde están ubicados',
-  'direccion', 'dirección',
-  'como llegar', 'cómo llegar',
-  '¿Hacen envíos?',
-  'envios',
-  'hacen envios',
-  'envios a domicilio',
-  'ubicacion', 'donde estan ubicados', 'direccion', 'como llegar',
-]).addAnswer('Estamos ubicados en Bogotá 😊:', { delay: 5000 })
-  .addAnswer([
-    'Bogotá - Barrio El Lago',
-    'Carrera 14 #80-61 (Divino Placer/Bendito Sex Store)',
-    'Lunes a Sábado: 9:30 A.M a 9:30 P.M',
-    'Domingos y festivos: 12:30 P.M a 7:30 P.M'
-  ], {
-    media: 'https://lh3.googleusercontent.com/p/AF1QipO_0WIAD9JXh3Ii534fg_iM-W44WU3yiVb04Zg=s680-w680-h510'
-  }).addAnswer('¿Desde que ciudad nos hablas?', { delay: 2000, capture: true })
+  "donde estan ubicados",
+  "dónde están ubicados",
+  "ubicacion",
+  "ubicación",
+  "direccion",
+  "dirección",
+  "¿Hacen envíos?",
+  "envios",
+  "hacen envios",
+  "envios a domicilio",
+  "ubicacion",
+  "ubicación",
+  "donde estan ubicados",
+  "dónde están ubicados",
+  "direccion",
+  "dirección",
+  "como llegar",
+  "cómo llegar",
+  "¿Hacen envíos?",
+  "envios",
+  "hacen envios",
+  "envios a domicilio",
+  "ubicacion",
+  "donde estan ubicados",
+  "direccion",
+  "como llegar",
+])
+  .addAnswer("Estamos ubicados en Bogotá 😊:", { delay: 5000 })
+  .addAnswer(
+    [
+      "Bogotá - Barrio El Lago",
+      "Carrera 14 #80-61 (Divino Placer/Bendito Sex Store)",
+      "Lunes a Sábado: 9:30 A.M a 9:30 P.M",
+      "Domingos y festivos: 12:30 P.M a 7:30 P.M",
+    ],
+    {
+      media:
+        "https://lh3.googleusercontent.com/p/AF1QipO_0WIAD9JXh3Ii534fg_iM-W44WU3yiVb04Zg=s680-w680-h510",
+    },
+  )
+  .addAnswer("¿Desde que ciudad nos hablas?", { delay: 2000, capture: true })
   .addAction(async (ctx, { flowDynamic, state }) => {
     const city: string = ctx.body.toLowerCase();
     await state.update({ city });
 
-    if (city === 'bogota' || city === 'bogotá') {
-      return await flowDynamic('Ayúdanos con tu dirección para cotizar el envío 🚚', { delay: 5000 });
+    if (city === "bogota" || city === "bogotá") {
+      return await flowDynamic(
+        "Ayúdanos con tu dirección para cotizar el envío 🚚",
+        { delay: 5000 },
+      );
     } else {
-      return await flowDynamic(`Los pedidos a ${ctx.body} llegan en 24 a 48 horas hábiles.`, { delay: 5000 });
+      return await flowDynamic(
+        `Los pedidos a ${ctx.body} llegan en 24 a 48 horas hábiles.`,
+        { delay: 5000 },
+      );
     }
   });
 
 const webpageFlow = addKeyword<Provider, Database>([
-  'web',
-  'pagina', 'página',
-  'sitio web',
-  'redes sociales'
-]).addAnswer('Puedes visitar nuestra página web en el siguiente enlace: https://divinoplacer.com.co', { delay: 2000 });
+  "pagina",
+  "página",
+  "redes sociales",
+]).addAnswer(
+  "Puedes visitar nuestra página web en el siguiente enlace: https://divinoplacer.com.co",
+  { delay: 2000 },
+);
 
 // Inicialización del bot
 const main = async () => {
